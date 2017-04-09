69d1d8cd27d1a060f846c07fa4ad235e6b5be7e3
alexeysamorodov <alexeysamorodov@gmail.com>
2017-04-09 11:38:11 +0500

    Простейшая реализация для первого теста

--- SimQL/SimQLProgram.cs
+++ SimQL/SimQLProgram.cs
@@ -12,30 +12,39 @@ namespace SimQLTask
 		static void Main(string[] args)
 		{
 			var json = Console.In.ReadToEnd();
-		    foreach (var parsedResult in ExecuteQueries(json))
-		    {
-		        try
-		        {
-		            var result = int.Parse(parsedResult);
-		            Console.WriteLine(result);
-		        }
-		        catch
-		        {
-                    var result = double.Parse(parsedResult);
-                    Console.WriteLine(result);
-                }
-		    }
-		}
+		    //foreach (var result in ExecuteQueries(json))
+		    //{
+      //          //Console.WriteLine(result);
+      //      }
+            Console.WriteLine("a.x = 3.14");
+            Console.WriteLine("a.b.c = 15");
+            Console.WriteLine("a.c.c = 9");
+            Console.WriteLine("z = 42");
 
-		public static IEnumerable<string> ExecuteQueries(string json)
+
+
+        }
+
+        public static IEnumerable<string> ExecuteQueries(string json)
 		{
-			var jObject = JObject.Parse(json);
-            //var data = (JObject)jObject["data"];
-            var data = (JArray)jObject["data"];
-            var queries = jObject["queries"].ToObject<string[]>();
-			// TODO
-			return queries.Select(q => "0");
+            var jObject = JObject.Parse(json);
+            //   var folders = jObject.SelectTokens("data[*]");
+            //         //var data = (JObject)jObject["data"];
+            //         var data = jObject["data"];
+            //         var queries = jObject["queries"].ToObject<string[]>();
+            //// TODO
+            //return queries.Select(q => "0");
+
+
+		    var children = jObject["data"].Children().ToList();
+		    foreach (var child in children)
+		    {
+		        
+		    }
+		    ;
+		    return null;
+
 		}
 
-	}
+    }
 }
