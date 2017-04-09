f5f31561258a6208cbf839080c44d5706b18e00a
Yaroslav <skort43@gmail.com>
2017-04-09 15:04:52 +0500

    Json2

--- JsonConversion/ProductV2.cs
+++ JsonConversion/ProductV2.cs
@@ -2,11 +2,20 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace JsonConversion
 {
     class ProductV2
     {
+        [JsonConstructor]
+        public ProductV2(string name, string price, int count)
+        {
+            this.name = name;
+            this.price = price;
+            this.count = count;
+        }
+
         public string name { get; set; }
         public string price { get; set; }
         public int count { get; set; }
--- JsonConversion/V2Data.cs
+++ JsonConversion/V2Data.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace JsonConversion
 {
     class V2Data
     {
+        [JsonConstructor]
+        public V2Data(Dictionary<string, double> constants, string version, Dictionary<int, ProductV2> products)
+        {
+            this.constants = constants;
+            this.version = version;
+            this.products = products;
+        }
+
         public Dictionary<string,double> constants { get; set; }
         public string version { get; set; }
         public Dictionary<int,ProductV2> products { get; set; }
